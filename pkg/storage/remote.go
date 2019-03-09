bd9ccd245ffa184cbffca7c792ee107a33c2c06f
Jeremy Chambers <jeremy@thehipbot.com>
2019-03-09 10:21:39 -0600

    Refactor storage object to store repos in a map instead of array, Sort map keys on storage.SearchRepositories so that user is presented with an alphabetically sorted list of repos

--- pkg/storage/remote.go
+++ pkg/storage/remote.go
@@ -2,10 +2,10 @@ package storage
 
 // Remote is a parent node in the cache tree
 type Remote struct {
-	Name     string            `json:"name"`
-	URL      string            `json:"url"`
-	Protocol string            `json:"protocol"`
-	Type     string            `json:"type"`
-	Meta     map[string]string `json:"meta"`
-	Repos    []Repository      `json:"repos"`
+	Name     string                 `json:"name"`
+	URL      string                 `json:"url"`
+	Protocol string                 `json:"protocol"`
+	Type     string                 `json:"type"`
+	Meta     map[string]string      `json:"meta"`
+	Repos    map[string]*Repository `json:"repos"`
 }
